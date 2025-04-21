5ea464f631443098d092d9708da8385238cd9496
Michael Pierce <mtp@ncm20.com>
2025-04-21 13:55:03 -0300

    Created WeatherCondition

---
+++ SpaceMissionControl.Model/WeatherCondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMissionControl.Model
+{
+	public class WeatherCondition
+	{
+		public double WindSpeed { get; set; }
+		public double Temperature { get; set; }
+		public double Visibility { get; set; }
+		public bool Lightning { get; set; }
+	}
+}
