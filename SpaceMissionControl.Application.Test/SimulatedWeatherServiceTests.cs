4d0c0f6a442abe265ffb32a1b5d5951544e922a5
Michael Pierce <mtp@ncm20.com>
2025-05-06 11:55:07 -0300

    Created test class for SimulatedWeatherService

---
+++ SpaceMissionControl.Application.Test/SimulatedWeatherServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using SpaceMissionControl.Application.Weather;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceMissionControl.Application.Test
+{
+	[TestClass]
+	public class SimulatedWeatherServiceTests
+	{
+		private IWeatherService _weatherService;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_weatherService = new SimulatedWeatherService();
+		}
+
+		// TODO Create tests for the .IsSafeToLaunch method
+		// IsSafeToLaunch takes a WeatherCondition
+		// A WeatherCondition is safe to launch if it meets ALL of the following requirements
+		// -- WindSpeed is less than 30
+		// -- Visibility is greater than 3
+		// -- No lightning
+		// -- Temperature is greater than or equal to -10, and less than or equal to 35.
+		// Create a test for a valid, safe WeatherCondition. Then create tests for unsafe conditions
+		// Think about boundaries (for example, WindSpeed is less than 30 - what if WindSpeed IS 30?)
+	}
+}
